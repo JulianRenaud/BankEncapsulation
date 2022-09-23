7b0036a4d97cb5579ebc0eed27627c56701bac57
JulianRenaud <midway6220@gmail.com>
2022-09-23 14:04:11 -0500

    First Commit

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private static double balance = 0;
+        public static double Deposit(double deposit)
+        {
+            return balance += deposit;
+        }
+        public static double Withdraw(double withdraw)
+        {
+            
+                return balance -= withdraw;
+
+        }
+
+
+        public static void GetBalance()
+        {
+            
+            
+            Console.WriteLine(balance);
+        }
+
+
+
+    }
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,59 @@
+﻿using BankEncapsulation;
+using System.Security.Cryptography.X509Certificates;
+
+Console.WriteLine("Hello new customer, how much money would you like to deposit? ");
+double depositt = Convert.ToDouble(Console.ReadLine());
+BankAccount.Deposit(depositt);
+
+Console.WriteLine("----------------------------------------");
+
+
+Console.WriteLine("How much money do you need? ");
+double withdraww = Convert.ToDouble(Console.ReadLine());
+
+if (withdraww > depositt)
+{
+    Console.WriteLine($"");
+}
+else
+{
+    BankAccount.Withdraw(withdraww);
+}
+
+
+
+
+
+
+Console.WriteLine("----------------------------------------");
+
+
+
+
+
+if (withdraww > depositt)
+{
+    Console.WriteLine("You Owe");
+    Console.WriteLine($" {withdraww - depositt}");
+    Console.WriteLine("");
+}
+else
+{
+    Console.Write("You have ");
+    BankAccount.GetBalance();
+    Console.WriteLine("Dollars");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
